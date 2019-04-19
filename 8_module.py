de51ae977d68d4b3e0ded8e19065e4bb9ba634b5
tangxin20 <270474352@qq.com>
2019-04-20 07:27:20 +0800

    Create 8_module.py

---
+++ 8_module.py
@@ -0,0 +1,34 @@
+#import math
+#r = math.pow(2, 64)
+#print(r)
+
+#import random
+#r = random.randint(0, 100)
+#print(r)
+
+##均值
+#import statistics
+#nums = [1, 2, 3, 6]
+#r = statistics.mean(nums)
+#print(r)
+#
+##中值
+#r2 = statistics.median(nums)
+#print(r2)
+
+##众数
+#import statistics
+#nums = [1, 5, 33, 12, 46, 33, 2]
+#r3 = statistics.mode(nums)
+#print(r3)
+
+#import keyword
+#r = keyword.iskeyword("elif")
+#print(r)
+
+#import Login_Pram
+#Login_Pram.login_oa()
+
+import temp
+
+
