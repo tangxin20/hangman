e4ff24364cdf9cfdf81dcc6d39e8a1f0b704b6da
tangxin20 <270474352@qq.com>
2019-04-20 07:27:57 +0800

    Create if.py

---
+++ if.py
@@ -0,0 +1,49 @@
+#x = 100
+#if x == 10:
+#    print("10!")
+#elif x == 100:
+#    print("100!!")
+#else:
+#    print("I don't kown!")
+#
+#if x == 100:
+#    print("x is 100!")
+#if x%2 == 0:
+#    print("x is even!")
+#else:
+#    print("x is odd!")
+#print("12345")
+#print("hello world")
+#print("$$###)1)_!&")
+
+#a = float(input ("Please input a number:"))
+#if a <= 10:
+#    print("the number <= 10")
+#elif a > 10 and a <= 25:
+#    print("The number is :" + str(a))
+#else:
+#    print("the number > 25")
+
+#a = int(input("please input the first int number:"))
+#b = int(input("please input the second int number:"))
+#print(str(a)+"%"+str(b)+"="+str(a%b))
+
+#a = 15
+#b = 5
+#print(a//b)
+
+#a = float(input("Pleas input a int number:"))
+#if  a < 60 and a >= 0:
+#    print("你的得分为" + str(a) + ",成绩:不及格!")
+#elif  a >= 60 and a < 80:
+#    print("你的得分为" + str(a) + ",成绩:良好!")
+#elif  a >= 80 and a <= 100:
+#    print("你的得分为" + str(a) + ",成绩:优秀!")
+#else:
+#    print("你输入的成绩不规范，请输入0到100区间的整数")
+
+user = input("Please input username:")
+password = input("input the password")
+dict = {}
+dict[user]=password
+print(dict)
